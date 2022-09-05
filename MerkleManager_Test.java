0b4507ef1d2ce8c42baa4d302bfdb1e8dcb215ef
NoahBakayou <100172278+NoahBakayou@users.noreply.github.com>
2022-09-05 14:18:26 -0700

    Create MerkleManager_Test.java

---
+++ MerkleManager_Test.java
@@ -0,0 +1,15 @@
+//Noah Bakayou, Julian Stinnett, Dominic Devera, Aaron Anthony Yubac
+
+/* Note: Function is a code that has to play a role (a function) of doing something.
+   Method is a method to resolve the problem.
+   It does the same thing. */
+public class MerkleManager_Test {
+
+    public static void main(String[] args) {
+        MerkleManager oMerkleManager1 = new MerkleManager(); //instantiating instance of MerkleManager
+        oMerkleManager1.manage();
+
+    }
+
+
+}
