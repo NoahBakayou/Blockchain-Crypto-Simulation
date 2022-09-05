ef32a6b75a67bc1d43c0c1750adb7ab602d0e60f
NoahBakayou <100172278+NoahBakayou@users.noreply.github.com>
2022-09-05 14:16:42 -0700

    Create MerkleManager.java

---
+++ MerkleManager.java
@@ -0,0 +1,43 @@
+public class MerkleManager {
+    public static volatile String userEnteredWord; //value of variable must never be cached
+    public static String userEnteredExpectedMerkleRoot;
+    public static String merkleRoot = null;
+    public static int strikes = 0;
+
+
+    public void manage() {
+
+
+        //https://www.w3resource.com/java-tutorial/java-defining-instantiating-and-starting-threads.php
+        //instantiate
+        MerkleThread instanceMerkle = new MerkleThread();
+        RogueThread instanceRogue = new RogueThread();
+        MonitorThread instanceMonitor = new MonitorThread();
+
+        // new threads
+        Thread merkleThread = new Thread(instanceMerkle);
+        Thread rogueThread = new Thread(instanceRogue);
+        Thread monitorThread = new Thread(instanceMonitor);
+
+        merkleThread.start();
+        rogueThread.start();
+        monitorThread.start();
+
+        Util util1 = new Util();
+
+        while (true) {
+
+            MerkleManager.userEnteredWord = Util.promptUser("Enter the word: "); //need prompt user method in Util
+        }
+    }
+
+    public static synchronized String grabWord() {
+
+
+        String temp = userEnteredWord = null; //means user entered word is set to strign temp which == null
+
+        return temp; //returns string
+
+    }
+
+}
