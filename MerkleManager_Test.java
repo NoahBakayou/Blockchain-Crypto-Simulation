87dfa0efbb15ae4415ad0a2c0f5f8fca4d9c5c8e
NoahBakayou <100172278+NoahBakayou@users.noreply.github.com>
2022-09-12 19:30:30 -0700

    Update MerkleManager_Test.java

--- MerkleManager_Test.java
+++ MerkleManager_Test.java
@@ -1,12 +1,9 @@
 /*************************************
-
   Lab 2 CSIS 294:
-
   Noah Bakayou
   Julian Stinnett
   Dominic Devera
   Aaron Anthony Yubac
-
 ************************************/
 
 
